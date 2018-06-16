489f855f64050b0cdfd039f0adfe1b55f7f0ba34
David Tchepak <dave@davesquared.net>
2018-06-16 21:24:13 +1000

    Updating commented code in Ex03_Result for Swift 4.1

--- SwiftFpExercisesTests/Ex03_Result.swift
+++ SwiftFpExercisesTests/Ex03_Result.swift
@@ -28,6 +28,17 @@ public enum Result<E,A> : CustomStringConvertible {
         case .Value(let v): return onValue(v)
         }
     }
+    
+    // Helper property and functions for test assertions. You can safely ignore these.
+    public var description : String {
+        return self.reduce({ e in "Error: \(e)" }, { v in "Value: \(v)" })
+    }
+    public func getValue() -> A? {
+        return self.reduce({ _ in .none }, { a in .some(a) })
+    }
+    public func getError() -> E? {
+        return self.reduce({ e in .some(e) }, { _ in .none })
+    }
 }
 
 func success<E,A>(_ value : A) -> Result<E,A> {
@@ -37,82 +48,83 @@ func failure<E,A>(_ err : E) -> Result<E,A> {
     return .Error(err)
 }
 
-// TODO: Uncomment code block below and get the tests to pass:
-// ============================================================================================
+//// TODO: Uncomment the code below (to the end of the file) and get the tests to pass:
+//// ============================================================================================
 //extension Result {
-//    public func map<B>(f : A -> B) -> Result<E,B> {
+//    public func map<B>(_ f : (A) -> B) -> Result<E,B> {
 //        // *** TODO ***
-//        fatalError("*** TODO ***");
+//        fatalError("*** TODO ***")
 //    }
-//    
-//    public func flatMap<B>(f : A -> Result<E,B>) -> Result<E, B> {
+//
+//    public func flatMap<B>(_ f : (A) -> Result<E,B>) -> Result<E, B> {
 //        // *** TODO ***
-//        fatalError("*** TODO ***");
+//        fatalError("*** TODO ***")
 //    }
 //}
-//
 //
 //class Ex03_ResultExamples: XCTestCase {
 //    func testMapOverValue() {
 //        let result : Result<String,Int> = success(41).map({ x in x + 1 })
-//        
-//        XCTAssert(result.getValue() == .Some(42), result.description)
+//
+//        XCTAssert(result.getValue() == .some(42), result.description)
 //    }
 //    func testMapOverError() {
 //        let result : Result<String,Int> = failure("Something went wrong").map({ x in x + 1 })
-//        
-//        XCTAssert(result.getError() == .Some("Something went wrong"), result.description)
+//
+//        XCTAssert(result.getError() == .some("Something went wrong"), result.description)
 //    }
-//    
-//    func parseInt(s : String) -> Result<String, Int> {
-//        switch s.toInt() {
-//        case .Some(let x): return success(x)
-//        case .None: return failure("Could not convert '\(s)' to int")
+//
+//    func parseInt(_ s : String) -> Result<String, Int> {
+//        switch Int(s) {
+//        case .some(let x): return success(x)
+//        case .none: return failure("Could not convert '\(s)' to int")
 //        }
 //    }
 //
-//    func between(#min : Int, max :Int)(x : Int) -> Result<String, Int> {
-//        if x >= min && x <= max {
-//            return success(x)
-//        } else {
-//            return failure("Expected between \(min) and \(max), but was \(x)")
+//    func between(min : Int, max :Int) -> (Int) -> Result<String, Int> {
+//        return { x in
+//            if x >= min && x <= max {
+//                return success(x)
+//            } else {
+//                return failure("Expected between \(min) and \(max), but was \(x)")
+//            }
 //        }
 //    }
-//    
-//    func parseIntBetweenZeroAndTen(s : String) -> Result<String,Int> {
+//
+//    func parseIntBetweenZeroAndTen(_ s : String) -> Result<String,Int> {
 //        return parseInt(s).flatMap(between(min: 0, max: 10))
 //    }
-//    
+//
 //    func testFlatMap() {
 //        let validResult = parseIntBetweenZeroAndTen("5")
 //        let invalidIntResult = parseIntBetweenZeroAndTen("lots")
 //        let outOfRangeResult = parseIntBetweenZeroAndTen("42")
-//        
-//        XCTAssert(validResult.getValue() == .Some(5), validResult.description)
-//        XCTAssert(invalidIntResult.getError() == .Some("Could not convert 'lots' to int"), invalidIntResult.description)
-//        XCTAssert(outOfRangeResult.getError() == .Some("Expected between 0 and 10, but was 42"), outOfRangeResult.description)
+//
+//        XCTAssert(validResult.getValue() == .some(5), validResult.description)
+//        XCTAssert(invalidIntResult.getError() == .some("Could not convert 'lots' to int"), invalidIntResult.description)
+//        XCTAssert(outOfRangeResult.getError() == .some("Expected between 0 and 10, but was 42"), outOfRangeResult.description)
 //    }
-//    
+//
 //    //*** TODO ***
 //    //Using testFlatMap() as an example, write a function that attempts to parse an even number between 0 and 100 from a string.
 //    //Consider writing a function `even : Int -> Result<String, Int>` to help.
-//    func evenFrom0To100(s : String) -> Result<String, Int> {
+//    func evenFrom0To100(_ s : String) -> Result<String, Int> {
 //        return failure("*** TODO ***");
 //    }
-//    
+//
 //    func testEvenFrom0To100() {
 //        let validResult = evenFrom0To100("42")
 //        let invalidIntResult = evenFrom0To100("lots")
 //        let outOfRangeResult = evenFrom0To100("102")
 //        let oddResult = evenFrom0To100("43")
-//        
-//        XCTAssert(validResult.getValue() == .Some(42), validResult.description)
-//        XCTAssert(invalidIntResult.getError() == .Some("Could not convert 'lots' to int"), invalidIntResult.description)
-//        XCTAssert(outOfRangeResult.getError() == .Some("Expected between 0 and 100, but was 102"), outOfRangeResult.description)
-//        XCTAssert(oddResult.getError() == .Some("Expected even number, but was 43"), oddResult.description)
+//
+//        XCTAssert(validResult.getValue() == .some(42), validResult.description)
+//        XCTAssert(invalidIntResult.getError() == .some("Could not convert 'lots' to int"), invalidIntResult.description)
+//        XCTAssert(outOfRangeResult.getError() == .some("Expected between 0 and 100, but was 102"), outOfRangeResult.description)
+//        XCTAssert(oddResult.getError() == .some("Expected even number, but was 43"), oddResult.description)
 //        // Note: these tests don't specify what needs to be checked first: evenness or range. Either way is fine for this exercise.
 //    }
-//  
+//
 //    // EXTENSIONS:
 //    // 1. Create a function `>>-` which is an alias for Result.flatMap (the operator is already declared in Operators.swift).
 //    //        >>- : (Result<E,A>, A -> Result<E,B>) -> Result<E,B>
@@ -122,20 +134,5 @@ func failure<E,A>(_ err : E) -> Result<E,A> {
 //    //        >=> : (A -> Result<E,B>, B -> Result<E,C>) -> (A -> Result<E,C>)
 //    //    Re-write evenFrom0To100 using this operator.
 //}
-// ============================================================================================
+//// ============================================================================================
 
-
-
-
-// Helper functions for testing. Feel free to ignore.
-extension Result {
-    public func getValue() -> A? {
-        return self.reduce({ _ in .none }, { a in .some(a) })
-    }
-    public func getError() -> E? {
-        return self.reduce({ e in .some(e) }, { _ in .none })
-    }
-    public var description : String {
-        return self.reduce({ e in "Error: \(e)" }, { v in "Value: \(v)" })
-    }
-}
